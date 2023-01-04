32894958f79b4cf11cbfec7ae4b48d1bf7db6713
Jhemes Tony Rosante Caetano <jhemes.seker@gmail.com>
2023-01-04 12:09:53 -0300

    Update exercicio_02_58.py

--- Aula_14/exercicio_02_58.py
+++ Aula_14/exercicio_02_58.py
@@ -3,18 +3,23 @@
 import random
 import time
 print("Tente advinhar qual é numero escolhido pelo computador")
-print("Ele está entre o numero 0 até o 10")
-number_cpu = random.randrange(0, 11)
-number_player = 11
+print("Ele está entre o numero 1 até o 10")
+number_cpu = random.randrange(1, 11)
+number_player = 0
 tentavias = 1
 print(number_cpu)
 while number_cpu != number_player:
     number_player = int(input("Digite sua jogada:"))
-    if number_cpu != number_player:
-        tentavias += 1
-        time.sleep(0.2)
-        print(f'Desculpa você errou , Tente novamente')
     if number_cpu == number_player:
         time.sleep(0.2)
         print('PARABÉNS , Você acertou!!!')
+    else:
+        if number_player < number_cpu:
+            time.sleep(0.2)
+            print("mais... Tenta mais uma vez...")
+            tentavias += 1
+        elif number_player > number_cpu:
+            time.sleep(0.2)
+            print('menos... Tente mais uma vez')
+            tentavias += 1
 print(f'foram necesario um total de,"{tentavias}" tentavivas para vc ganhar ^.^')
